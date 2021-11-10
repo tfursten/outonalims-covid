8d7085affc399df1952e94a12fc57a44ce483e04
Tara Furstenau <tarafurstenau@Taras-MacBook-Pro.local>
2021-11-10 01:08:06 -0700

    update

--- out_on_a_LIMS/settings.py
+++ out_on_a_LIMS/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'lims'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
