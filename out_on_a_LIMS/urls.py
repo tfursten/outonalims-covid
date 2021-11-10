4da3ffd61a9935f67cb66cb77abe8fab28ca7d5f
Tara Furstenau <tarafurstenau@Taras-MacBook-Pro.local>
2021-11-10 01:41:08 -0700

    add index

--- out_on_a_LIMS/urls.py
+++ out_on_a_LIMS/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', auth_views.LoginView.as_view(), name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
