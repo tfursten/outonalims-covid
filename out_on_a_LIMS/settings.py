bb939cd1265fb3d078093e979f3df1ca429c8c32
Tara Furstenau <tarafurstenau@Taras-MacBook-Pro.local>
2021-11-10 00:53:01 -0700

    su

--- out_on_a_LIMS/settings.py
+++ out_on_a_LIMS/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',   
+    'django.contrib.staticfiles',
+    'lims'
 ]
 
 MIDDLEWARE = [
--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -1,6 +1,6 @@
+import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-import os
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
